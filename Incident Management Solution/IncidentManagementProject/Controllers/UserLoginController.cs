384fe176633ff4b81168ea3615d32827c9b1b582
SeemaPatil11182 <59469774+SeemaPatil11182@users.noreply.github.com>
2020-06-05 18:12:15 +0530

    Added Login page.

---
+++ Incident Management Solution/IncidentManagementProject/Controllers/UserLoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IncidentManagementProject.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+namespace IncidentManagementProject.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginClass lc)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            SqlCommand sqlcomm = new SqlCommand("user_login");
+            sqlconn.Open();
+            sqlcomm.Connection = sqlconn;
+            sqlcomm.CommandType = CommandType.StoredProcedure;
+            sqlcomm.Parameters.AddWithValue("@employee_mail_id", lc.UserName);
+            sqlcomm.Parameters.AddWithValue("@password", lc.Password);
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            if (sdr.Read())
+            {
+                FormsAuthentication.SetAuthCookie(lc.UserName, true);
+                Session["username"] = lc.UserName.ToString();
+                return RedirectToAction("welcome");
+            }
+            else
+            {
+
+                ViewData["message"] = "Login failed, Invalid User details";
+            }
+            sqlconn.Close();
+            return View();
+        }
+        public ActionResult welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ Incident Management Solution/IncidentManagementProject/Models/LoginClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace IncidentManagementProject.Models
+{
+    public class LoginClass
+    {
+        [EmailAddress(ErrorMessage = "Email Address not valid !")]
+        [Required(ErrorMessage = "Please enter your Username !")]
+        [Display(Name = "Enter Username :")]
+
+        public string UserName
+        {
+            get; set;
+        }
+
+        [Required(ErrorMessage = "Please enter your Password !")]
+        [Display(Name = "Enter Password :")]
+        [MinLength(5)]
+        [DataType(DataType.Password)]
+
+        public string Password
+        {
+            get; set;
+        }
+
+    }
+}
+    
